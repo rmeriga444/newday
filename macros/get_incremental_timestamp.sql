1da2df6f3a61d673d30906a9355b8ba896bf5fc0
rmeriga444 <rmerigausw@gmail.com>
2026-04-09 01:28:30 -0400

    Add files via upload

---
+++ macros/get_incremental_timestamp.sql
@@ -0,0 +1,14 @@
+{% macro get_incremental_timestamp(source_ts_col, watermark_col=none) %}
+
+  {%- set wm_col = watermark_col if watermark_col is not none else source_ts_col -%}
+
+  {% if is_incremental() %}
+
+    where {{ source_ts_col }} > (
+        select coalesce(max({{ wm_col }}), '1900-01-01'::timestamp)
+        from {{ this }}
+    )
+
+  {% endif %}
+
+{% endmacro %}
